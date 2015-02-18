96fb740ed05babe1839696a58f605ccbb8020334
Boris Vujicic <turshija@gmail.com>
2015-02-18 14:56:07 +0100

    Added navigation with options

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 include "lib/rebel_parser.class.php";
 $rebel = new RebelParser();
-
-$task = "make_changes";
+?>
+<a href="?task=check_mismatch">Check files for arguments mismatch</a>
+<br />
+<a href="?task=show_changes">Show changes</a>
+<br />
+<a href="?task=make_changes">Make changes (WRITE, beware!)</a>
+<hr />
+<?php
+if (isset($_GET['task'])) $task = $_GET['task'];
+else $task = "check_mismatch";
 
 switch ($task) {
     case "check_mismatch": include "arguments_mismatch.php";break;
     case "show_changes": include "show_changes.php";break;
     case "make_changes": include "make_changes.php";break;
 }
-
